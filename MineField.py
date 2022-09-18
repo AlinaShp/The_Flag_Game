6fa09b93ba5a940b2fc906a7eba409ee278acb08
Michal Eretz Kdosha <mik2004ek@gmail.com>
2022-09-18 10:10:57 +0300

    create main function

--- MineField.py
+++ MineField.py
@@ -3,14 +3,14 @@ import random
 MINE_FIELD_MATRIX = []
 
 def create_mine_field():
-    temp_list = []
-    for col in consts.SCREEN_WIDTH:
-        temp_list.append([consts.FREE])
-    for row in consts.SCREEN_HEIGHT:
-        MINE_FIELD_MATRIX.append(temp_list)
-
-    for i in consts.NUM_LAND_MINES:
-        MINE_FIELD_MATRIX[random.randint(0,consts.SCREEN_HEIGHT)][random.randint(0,consts.SCREEN_WIDTH)] = consts.MINE_FILE
+    # temp_list = []
+    # for col in consts.SCREEN_WIDTH:
+    #     temp_list.append([consts.FREE])
+    # for row in consts.SCREEN_HEIGHT:
+    #     MINE_FIELD_MATRIX.append(temp_list)
+    #
+    # for i in consts.NUM_LAND_MINES:
+    #     MINE_FIELD_MATRIX[random.randint(0,consts.SCREEN_HEIGHT)][random.randint(0,consts.SCREEN_WIDTH)] = consts.MINE_FILE
     return MINE_FIELD_MATRIX
 
 
--- main.py
+++ main.py
@@ -2,65 +2,22 @@ import pygame
 import screen
 import consts
 import Soldier
-FPS = 60
-"""black = (0, 0, 0)
-white = (255, 255, 255)
+import MineField
 
-red = (255, 0, 0)
-WIDTH = 20
-HEIGHT = 20
-MARGIN = 5
-grid = []
-for row in range(10):
-    grid.append([])
-    for column in range(10):
-        grid[row].append(0)
-grid[1][5] = 1
-pygame.init()
-window_size = [1000, 500]
-scr = pygame.display.set_mode(window_size)
-pygame.display.set_caption("Grid")
-done = False
-clock = pygame.time.Clock()
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            column = pos[0] // (WIDTH + MARGIN)
-            row = pos[1] // (HEIGHT + MARGIN)
-            grid[row][column] = 1
-            print("Click ", pos, "Grid coordinates: ", row, column)
-    scr.fill(black)
-    for row in range(10):
-        for column in range(10):
-            color = white
-            if grid[row][column] == 1:
-                color = red
-            pygame.draw.rect(scr,
-                             color,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row + MARGIN,
-                              WIDTH,
-                              HEIGHT])
-    clock.tick(50)
-    pygame.display.flip()
-pygame.quit()"""
+state = {'state': True, 'mine_field': None, 'grass': None, 'soldier_rect': None}
 
 
 def main():
     clock = pygame.time.Clock()
-    state = True
-    soldier_rect = pygame.Rect(0, 0, consts.PLAYER_SIZE[0],consts.PLAYER_SIZE[1])
-    while state:
-        clock.tick(FPS)
+    state['soldier_rect'] = pygame.Rect(0, 0, consts.PLAYER_SIZE[0],consts.PLAYER_SIZE[1])
+    while state['state']:
+        clock.tick(consts.FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                state = False
+                state['state'] = False
         key_pressed = pygame.key.get_pressed()
-        screen.screen_color_change(key_pressed, soldier_rect)
-        Soldier.soldier_move(key_pressed, soldier_rect)
+        screen.screen_color_change(key_pressed, state['soldier_rect'])
+        Soldier.soldier_move(key_pressed, state['soldier_rect'])
 
     pygame.quit()
 
