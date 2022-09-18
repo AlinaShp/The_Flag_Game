e7be4f10e65c579d9fb14d18dcc26de436f4bf2f
Michal Eretz Kdosha <mik2004ek@gmail.com>
2022-09-18 11:02:09 +0300

    add flag const

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import MineField
 
 state = {'state': True, 'mine_field': MineField.init_mine_field(), 'grass': None, 'soldier_rect': None}
 
+
 def main():
     clock = pygame.time.Clock()
     state['soldier_rect'] = pygame.Rect(0, 0, consts.PLAYER_SIZE[0],consts.PLAYER_SIZE[1])
