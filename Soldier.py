03a6f8219192f8954ba5bddaf57c44dddb060545
AlinaShp <alina9614999@gmail.com>
2022-09-18 14:26:47 +0300

    mines on field

--- Soldier.py
+++ Soldier.py
@@ -5,10 +5,12 @@ import consts
 def soldier_move(key_pressed, soldier_rect):
     if key_pressed[pygame.K_DOWN] and soldier_rect.y + consts.CELL_SIZE < consts.SCREEN_HEIGHT-60:
         soldier_rect.y += consts.CELL_SIZE
+
     if key_pressed[pygame.K_UP] and soldier_rect.y - consts.CELL_SIZE > 0:
         soldier_rect.y -= consts.CELL_SIZE
+
     if key_pressed[pygame.K_LEFT] and soldier_rect.x - consts.CELL_SIZE > 0:
         soldier_rect.x -= consts.CELL_SIZE
+
     if key_pressed[pygame.K_RIGHT] and soldier_rect.x + consts.CELL_SIZE < consts.SCREEN_WIDTH-20:
         soldier_rect.x += consts.CELL_SIZE
-       #soldier_rect.x += 5
