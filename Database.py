7c68a361d6cd6be1fccf0db21a4b4d582d577411
Michal Eretz Kdosha <mik2004ek@gmail.com>
2022-09-19 15:39:39 +0300

    Database

--- Database.py
+++ Database.py
@@ -3,14 +3,13 @@ import os
 import json
 
 DATA_BASE_NAME = 'dataBase.csv'
-DATA_FRAME = None
 
 
 def init_data_base():
     if not os.path.exists(DATA_BASE_NAME):
-        new_file = open(DATA_BASE_NAME, 'w')
-        new_file.write('key_pressed,mine_field,grass, soliderPosition\n')
-        new_file.close()
+        df = pandas.DataFrame()
+        df.to_csv(DATA_BASE_NAME, columns=['num_button_pressed','mine_field','grass','soliderPosition'], mode='w')
+
 
 
 def convert_data_lists_to_srt(game_data):
@@ -20,23 +19,25 @@ def convert_data_lists_to_srt(game_data):
     return game_data
 
 
-def save_game(game_data, num_button_pressed):
+def save_game(game_data):
     game_data = convert_data_lists_to_srt(game_data)
-    # df = pandas.read_csv(DATA_BASE_NAME)
-    # df.drop(index= df[df['key'] == num_button_pressed].index, inplace=True)
-    # df.to_csv(DATA_BASE_NAME)
-    new_data_frame = pandas.DataFrame([game_data])#, index=[num_button_pressed])
+    new_data_frame = pandas.DataFrame([game_data])
+    df = pandas.read_csv(DATA_BASE_NAME)
+    if (df['num_button_pressed'] == game_data['num_button_pressed']).any():
+        df.drop(index=df[df['num_button_pressed'] == game_data['num_button_pressed']].index, inplace=True)
+        df.to_csv(DATA_BASE_NAME, mode='w', header=False)
     new_data_frame.to_csv(DATA_BASE_NAME, mode='a', header=False)
 
 
 def get_game(num_button_pressed):
     df = pandas.read_csv(DATA_BASE_NAME)
-    df2 = df[df['key_pressed'] == num_button_pressed]
+    df2 = df[df['num_button_pressed'] == num_button_pressed]
     print(df2.to_string())
 
+
 init_data_base()
-save_game({'key_pressed': 4,'mine_field':[[1,1], [1,1]],'grass':[[1,1], [1,1]],'soliderPosition':[1,1]},0)
-save_game({'key_pressed':3, 'mine_field':[[2,2], [2,2]],'grass':[[2,2], [2,2]],'soliderPosition':[1,1]}, 1)
-get_game(0)
-DATA_FRAME = pandas.read_csv(DATA_BASE_NAME)
-print(DATA_FRAME)
+# save_game({'num_button_pressed':0,'mine_field':[[1,1], [1,1]],'grass':[[1,1], [1,1]],'soliderPosition':[1,1]})
+# save_game({'num_button_pressed':1,'mine_field':[[1,1], [1,1]],'grass':[[1,1], [1,1]],'soliderPosition':[1,1]})
+# # # #get_game(0)
+# # d = pandas.read_csv(DATA_BASE_NAME)
+# print(df)
